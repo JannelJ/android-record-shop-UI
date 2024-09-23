f0d2a25de77bbe5c667bbb04d69b2e8b0d7eb053
JannelJ <janneljones26@gmail.com>
2024-09-23 10:43:40 +0100

    feat: method to return mutablelivedata

--- app/src/main/java/com/northcoders/recordshopapp/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/recordshopapp/model/AlbumRepository.java
@@ -5,11 +5,25 @@ import android.app.Application;
 import androidx.lifecycle.MutableLiveData;
 
 import com.northcoders.recordshopapp.AlbumModel;
+import com.northcoders.recordshopapp.service.AlbumAPIService;
+import com.northcoders.recordshopapp.service.RetrofitInstance;
 
 import java.util.List;
+
+import retrofit2.Call;
 
 public class AlbumRepository {
 
     private MutableLiveData<List<AlbumModel>> mutableLiveData = new MutableLiveData<>();
     private Application application;
-}
+
+    public AlbumRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<AlbumModel>> getMutableLiveData() {
+        AlbumAPIService albumApiService = RetrofitInstance.getService();
+        Call<List<AlbumModel>> call = albumApiService.getAllAlbums();
+
+    }
+
