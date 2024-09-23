7f6c68bd835de94965c0b35ff6902deb109e77aa
JannelJ <janneljones26@gmail.com>
2024-09-23 16:09:02 +0100

    add context object and AlbumAdapter constructor for RecyclerView implementation

---
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/AlbumAdapter.java
@@ -0,0 +1,57 @@
+package com.northcoders.recordshopapp.ui.mainactivity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.northcoders.recordshopapp.R;
+import com.northcoders.recordshopapp.databinding.LayoutBinding;
+import com.northcoders.recordshopapp.model.AlbumModel;
+
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder> {
+
+    private List<AlbumModel> albumModelList;
+    private Context context;
+
+    public AlbumAdapter(Context context, List<AlbumModel> albumModelList) {
+        this.albumModelList = albumModelList;
+        this.context = context;
+    }
+
+    public static class AlbumViewHolder extends RecyclerView.ViewHolder {
+        private LayoutBinding layoutBinding;
+
+        public AlbumViewHolder(LayoutBinding layoutBinding) {
+            super(layoutBinding.getRoot());
+            this.layoutBinding = layoutBinding;
+        }
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.layout, parent, false);
+        return new AlbumViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        AlbumModel album = albumModelList.get(position);
+        holder.layoutBinding.setAlbumModel(album);
+    }
+
+    // Method to return the total number of items in the data list
+    @Override
+    public int getItemCount() {
+        return albumModelList.size();
+
+        }
+
+    }
+
--- app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivity.java
@@ -8,17 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.northcoders.recordshopapp.R;
+import com.northcoders.recordshopapp.databinding.ActivityMainBinding;
+import com.northcoders.recordshopapp.model.AlbumModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Application application = new Application();
-        MainActivityViewModel mainActivityViewModel = new MainActivityViewModel(application);
-        mainActivityViewModel.getAlbums();
+
+
+
+//        Application application = new Application();
+//        MainActivityViewModel mainActivityViewModel = new MainActivityViewModel(application);
+//        mainActivityViewModel.getAlbums();
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
